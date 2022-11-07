574e28b53cb6200fe4506aca8b249a7c15e479ea
gryder8 <gavin.ryder82@icloud.com>
2022-11-06 19:04:59 -0800

    Initial Commit

---
+++ Inrix Hack 2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Inrix Hack 2022
+//
+//  Created by Gavin Ryder on 11/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Inrix Hack 2022/Inrix_Hack_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Inrix_Hack_2022App.swift
+//  Inrix Hack 2022
+//
+//  Created by Gavin Ryder on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Inrix_Hack_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
