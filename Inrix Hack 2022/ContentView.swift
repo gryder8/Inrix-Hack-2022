bb34b6029a005e697a9eb1c89aa160ac6a57b6c0
gryder8 <gavin.ryder82@icloud.com>
2022-11-06 19:13:06 -0800

    Some skeleton

--- Inrix Hack 2022/ContentView.swift
+++ Inrix Hack 2022/ContentView.swift
@@ -6,13 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("INRIX Hack 2022!")
         }
         .padding()
---
+++ Inrix Hack 2022/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Inrix Hack 2022
+//
+//  Created by Gavin Ryder on 11/6/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
